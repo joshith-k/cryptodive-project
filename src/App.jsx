5230ce09218695c81884e2f993a5c4c7e56385ea
joshith-k <joshith143@gmail.com>
2025-08-31 15:49:47 +0530

    Changed imports and added missing sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Faq from "./section/Faq/Faq.jsx";
 import Footer from "./section/Footer/Footer.jsx";
 import Header from "./section/Header/Header.jsx";
-import Home from "./section/Home/home.jsx";
+import Home from "./section/Home/Home.jsx";
 import Learn from "./section/Learn/Learn.jsx";
 import Market from "./section/Market/Market.jsx";
 import Support from "./section/Support/Support.jsx";
